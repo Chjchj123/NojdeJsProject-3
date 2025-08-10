0da672204eb40782b073afd1ef410f1150876bfe
Chjchj123 <quanganh2004dasuo@gmail.com>
2025-08-10 21:30:58 +0700

    controller-update

--- src/routes/v1/boardRoutes.js
+++ src/routes/v1/boardRoutes.js
@@ -2,11 +2,12 @@ import express from 'express';
 const Router = express.Router();
 const { StatusCodes } = require('http-status-codes');
 const { boardValidation } = require('~/validations/boardValidation.js');
+const { boardController } = require('~/controllers/boardController.js');
 
 Router.route('/')
     .get((req, res) => {
         res.status(StatusCodes.OK).json({ message: 'APIs V1 request successful' });
     })
-    .post(boardValidation.createNew)
+    .post(boardValidation.createNew, boardController.createNew)
 
 export const BoardRoutes = Router
